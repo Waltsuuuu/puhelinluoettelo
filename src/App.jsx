0472558dc7604a3d0872f297251647f78529afcf
Waltsuuu <waltteriheino@gmail.com>
2024-09-17 12:31:04 +0300

    Moved persons data to db.jsonand modified app to fetch data using axios and useEffect hook, step6 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Components/FIlter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('') 
   const [searchQuery, setSearchQuery] = useState('')
+
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const normalizeNumber = (number) => {
     // Remove spaces, dashes, etc. for comparison
